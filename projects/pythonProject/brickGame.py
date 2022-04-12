8cbd6829880089410a492012fcbfabc2fcd914b8
Sarah Rockow <rockows@mail.gvsu.edu>
2022-04-11 20:19:26 -0400

    functional, but missing features

--- projects/pythonProject/brickGame.py
+++ projects/pythonProject/brickGame.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python3
 
+"""
+A version of the Brick Breaker game (https://www.coolmathgames.com/0-brick-breaker)
+that allows a user to play with one or more balls at different speeds using
+hidden keystrokes. To win, the user must defeat a grid of 48 bricks with varying
+health levels.
+
+@author             Sarah Rockow
+@collaborator       Amaka Ezuruonye
+@version            Winter 2022
+
+Notes:
+
+    The overlay is currently not working -> this will be fixed soon
+
+    Hidden keystrokes:
+        f - speeds up the ball(s)
+        j - slows down the ball(s)
+        down_arrow - adds another ball (unlimited amount)
+"""
+
+
+
 import pygame as pg
 from pygame import *
 import random
@@ -43,12 +65,15 @@ class Brick(pg.sprite.Sprite):
     and can take 3 hits before being destroyed
     """
     def update(self):
+
+        # checks if the ball has hit a brick and updates the health of the brick
         ballCollision = pg.sprite.spritecollide(self, Game.balls, False)
 
         if ballCollision:
-            self.__health -= 25
+            self.__health -= 250
             self.image.fill( (self.__red, self.__blue, self.__green) )
 
+        # removes brick when health is depleted
         if self.__health <= 0:
             # this will move the bricks off screen
             self.rect.x = -100
@@ -61,11 +86,6 @@ class Brick(pg.sprite.Sprite):
     def getHealth(self):
         return self.__health
 
-    """
-    a method to set brick health
-    """
-    def setHealth(self, value):
-        self.__health -= value
 
 """
 A constructor that creates a ball
@@ -74,21 +94,20 @@ class Ball(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
         
+        # the number of lives that each ball has
         self.__lives = 5
         self.__points = 0
 
-         # a random variable for direction
-        r = random.randint
-
+        # creates the ball
         self.image = pg.Surface((15, 15))
         self.rect = self.image.get_rect()
         
+        # positions the ball in the middle of the screen
         self.rect.x = 400
         self.rect.y = 300
 
-        # copying from instructor sample code
+        # initial velocity of the ball
         self.velocity = [4, 4]
-        #self.velocity = [ r(1, 5) - 5, r(1, 5) - 5]
 
     def update(self):
         # copying from instructor sample code
@@ -104,6 +123,15 @@ class Ball(pg.sprite.Sprite):
         if self.rect.y > 600:
             self.velocity[1] = -self.velocity[1]
 
+
+        # change ball speed when 'f' is pressed
+        if pg.key.get_pressed()[pg.K_f]:
+            self.velocity = [10, 10]
+
+        # slow down ball speed when 'j' is pressed
+        if pg.key.get_pressed()[pg.K_j]:
+            self.velocity = [2, 2]
+        
         # check if ball hits the ground and take away a life
         if self.rect.y == 0:
             self.__lives -= 1
@@ -113,6 +141,7 @@ class Ball(pg.sprite.Sprite):
             self.rect.x = -100
             self.rect.y = -100
             self.velocity = [0, 0]
+            
         # check for brick collision
         brickCollision = pg.sprite.spritecollide(self, Game.bricks, False)
 
@@ -174,8 +203,10 @@ class Paddle(pg.sprite.Sprite):
     def update(self):
         keys = pg.key.get_pressed()
 
-        self.rect.x += (keys[pg.K_RIGHT] - keys[pg.K_LEFT]) * 5
+        # move the paddle based on key binding
+        self.rect.x += (keys[pg.K_RIGHT] - keys[pg.K_LEFT]) * 15
         
+        # limits the paddle's movement
         if self.rect.x > 500:
             self.rect.x = 500
         if self.rect.x < 0:
@@ -190,6 +221,10 @@ class Overlay(pg.sprite.Sprite):
         super().__init__()
         print(Ball().getPoints())
 
+    def update(self):
+        Ball().getPoints()
+        print(Ball().getPoints())
+
 class Game:
     def __init__(self):
         pg.init()
@@ -197,6 +232,7 @@ class Game:
         self.screen = pg.display.set_mode( (800, 600) )
         self.clock = pg.time.Clock()
         
+        # assign attributes to Game() Class
         brick = None
         paddles = None
         balls = None
@@ -216,12 +252,21 @@ class Game:
         self.paddles = pg.sprite.Group()
         self.paddles.add(Paddle())
 
-        # set titles
+        # set title
         pg.display.set_caption("Brick Game")
+
+
+
+
+
+
 
         #scoreText = pg.font.Font.render(pg.font.SysFont, "Score: ")
         # Player's paddle
-        self.__paddle = Paddle()
+
+
+
+
 
         # initialize the overlay
         self.overlay = pg.sprite.Group()
@@ -236,16 +281,9 @@ class Game:
                     pg.quit()
                     exit()
 
-                if event.type == pg.KEYDOWN:
-                    if event.type == pg.K_DOWN:
-                        self.balls.add(Ball () )
-                        print("added ball")
-
                 # adds a new ball when the down arrow is pressed; no limit
                 if pg.key.get_pressed()[pg.K_DOWN]:
                     self.balls.add( Ball() )
-
-         # Take events
 
             # Update updateable objects
             self.balls.update()
@@ -262,32 +300,40 @@ class Game:
 
             pg.display.flip()
             self.clock.tick(60)
-
+    
+    # setter for Game loop
     def setRunning(self, running):
         self.__running = running
 
+    # method to add a Brick
     def addBrick(self, brick):
         self.brick.add(brick)
 
+    # method to add a Paddle
     def addPaddle(self, paddle):
         self.paddles.add(paddle)
 
+    # method to add a Ball
     def addBall(self, ball):
         self.balls.add(ball)
 
+    # method to return a Brick from the Game() Class
     def getBricks(self):
         return self.brick
 
+    # method to return a Paddle from the Game() Class
     def getPaddle(self):
         return self.paddles
 
+    # method to return a Ball from the Game() Class
     def getBall(self):
         return self.balls
 
 def main():
+    # initialize the Game() Class
     game = Game()
 
-    # give attributes to Game class
+    # give attributes to Game class for other classes to access
     Game.bricks = game.getBricks()
     Game.paddles = game.getPaddle()
     Game.balls = game.getBall()
@@ -296,6 +342,7 @@ def main():
     game.setRunning(True)
     game.run()
 
+# run main method
 if __name__ == '__main__':
     main()
 
