93e504af544a8073cc3f5066f759b3001569bf5b
srockow2000 <77978654+srockow2000@users.noreply.github.com>
2022-04-25 15:24:59 -0400

    Include exponentiation

--- projects/calculator/src/calc.ts
+++ projects/calculator/src/calc.ts
@@ -1,5 +1,5 @@
 console.log("Welcome to your TypeScript Calculator");
-console.log("You can perform the following operations: addition (+), subtraction (-), multiplication (*), and division (/)");
+console.log("You can perform the following operations: addition (+), subtraction (-), multiplication (*), division (/), and exponentiation (^).");
 
 function performCalculations(operation: any, x:any, y: any): any {
     if (operation == "+") {
@@ -21,37 +21,56 @@ function performCalculations(operation: any, x:any, y: any): any {
         }
         return (x / y)
     }
+
+    if (operation == "^") {
+        return x ** y;
+    }
 }
 
+
 const data = require('prompt-sync')();
+var lastCalc = "";
 
 var op;
 var x;
 var y;
+var result;
+
 
 while (1) {
-    console.log("\n\nHere was your last calculation: " +
-    x + " " + op + " " + y + " = " + performCalculations(op, x, y))
-    op = data("Choose an operation [+, -, *, /] or enter 'q': ")
+
+    if (lastCalc != "") {
+        console.log("\n\nHere was your last calculation: " +
+        x + " " + op + " " + y + " = " + lastCalc)
+    }
+    op = data("Choose an operation [+, -, *, /, ^] or enter 'q': ")
 
     if (op == "q") {
         console.log("\nHave a nice day!")
         break;
     }
 
-    if (op == "*" || op == "-" || op == "+" || op == "/") {
+    if (op == "*" || op == "-" || op == "+" || op == "/" || op == "^") {
 
     console.log("\nYou chose: " + op)
 
-    x = data("Enter your first number: ");
+    if (result == 'y') {
+        x = lastCalc;
+        console.log("Your first number is: " + x);
+    }
+    else {
+        x = data("Enter your first number: ");
+    }
     y = data("Enter your second number: ");
 
     console.log("You entered " + x + " and " + y);
-    console.log(x + " " + op + " " + y + " is equal to: " + 
-    performCalculations(op, x, y))
+    lastCalc = performCalculations(op, x, y);
+
+    console.log(x + " " + op + " " + y + " is equal to: " + lastCalc)
+
+    result = data("If you would like to use your result [" + lastCalc + "], enter 'y' ");
 }
 else {
     console.log("Please try again")
 }
 }
-
